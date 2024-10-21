04b39483cdeef761d6ddec13b6c32be618a12459
XoGojiOx <xogojiox@gmail.com>
2024-10-20 20:45:48 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Prompt user for the length of the rectangle
+        Console.WriteLine("Enter the length of the rectangle:");
+        string lengthInput = Console.ReadLine();
+        double length = Convert.ToDouble(lengthInput);
+
+        // Prompt user for the width of the rectangle
+        Console.WriteLine("Enter the width of the rectangle:");
+        string widthInput = Console.ReadLine();
+        double width = Convert.ToDouble(widthInput);
+
+        // Calculate the area
+        double area = length * width;
+
+        // Print the result
+        Console.WriteLine("The area of the rectangle is: " + area);
+    }
+}
+
